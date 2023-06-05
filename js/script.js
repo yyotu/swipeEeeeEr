c4c0ff43338c05861f741d50afb2da72d4f3cd23
yyotu <sokolovyne@mail.ru>
2023-06-05 23:11:19 +0300

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
